264a16acc80d3ac22ed6606f16d76acd5a692439
Cfval <francesvaldesc@gmail.com>
2025-11-10 09:48:54 +0100

    Initial Spring Boot project

---
+++ src/main/java/com/tfg/digitalcitizen/mobilemanagementplatform/MobileManagementPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.tfg.digitalcitizen.mobilemanagementplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileManagementPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileManagementPlatformApplication.class, args);
+	}
+
+}
